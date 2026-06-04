e5e5c1d895f6e5149f8e704545583e2681302066
amakor97 <amakor.andrei@gmail.com>
2026-06-04 15:08:40 +0300

    [UPDATE]: write addTask function

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import type { Store } from "./types/Store";
 import TasksList from "./components/TasksList/TasksList";
 import Button from "./components/Button/Button";
 
+import { testTask1 } from "./tests/PoC/task";
+
 function App() {
   const [store, setStore] = useState<Store>({
     tasksList: new Map()
   });
 
+  function addTask() {
+    setStore((store) => {
+      const newTasksList = new Map(store.tasksList);
+      newTasksList.set("1", testTask1);
+      return {
+        ...store,
+        tasksList: newTasksList
+      };
+    });
+  }
+
   return (
     <div>
-      <TasksList/>
+      <TasksList />
       <Button />
     </div>
   );
