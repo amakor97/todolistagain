f5690e892f66db595af8f0b2814dfc4785ead7bb
amakor97 <amakor.andrei@gmail.com>
2026-06-04 10:51:35 +0300

    [CONFIG]: update configs and fix strict root render error

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+const Root = document.getElementById("root");
+
+if (Root) {
+  createRoot(Root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
