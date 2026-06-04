e85734fc290258c04e9c72d14d1a1b083a08706c
amakor97 <amakor.andrei@gmail.com>
2026-06-04 11:17:48 +0300

    [UPDATE]: set useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+import type { Store } from "./types/Store";
+
 function App() {
+  const [store, setStore] = useState<Store>({
+    tasksList: new Map()
+  });
+
   return <div></div>;
 }
 
