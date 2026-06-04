68e5cb19f42355d25c14010a90610cf22a3ec987
amakor97 <amakor.andrei@gmail.com>
2026-06-04 14:51:44 +0300

    [TEST]: add PoC test task

---
+++ src/tests/PoC/task.ts
@@ -0,0 +1,8 @@
+import type { Task } from "../../types/Task";
+
+export const testTask1: Task = {
+  id: "135642",
+  name: "Test task 1",
+  desription: "Test description 1",
+  status: "awaiting"
+};
