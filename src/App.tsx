5fe3c30be09d2127161de6ae4b29f90e47471692
amakor97 <amakor.andrei@gmail.com>
2026-06-24 19:30:55 +0300

    [FEATURE]: implement changing parent tasks' statuses

--- src/App.tsx
+++ src/App.tsx
@@ -16,12 +16,8 @@ const initialState: Store = {
   addingSubtaskId: null
 };
 
-
-
 function App() {
   const [store, setStore] = useState<Store>(initialState);
-
-
 
   useEffect(() => {
     async function setData(): Promise<void> {
@@ -34,8 +30,19 @@ function App() {
     void setData();
   }, []);
 
+  useEffect(() => {
+    async function uploadData(): Promise<void> {
+      await loadData(store);
+    }
+
+    void uploadData();
+  }, [store]);
+
   function addTask(newTask: TaskType): void {
     changeMode("idle");
+
+    console.log("gg");
+
     setStore((store) => {
       const newTasksList = new Map(store.tasksList);
 
@@ -50,6 +57,8 @@ function App() {
 
       newTasksList.set(newTask.id, newTask);
 
+      console.log(newTask.id, newTask.status);
+
       const newStore = {
         ...store,
         tasksList: newTasksList,
@@ -58,6 +67,8 @@ function App() {
 
       void loadData(newStore);
 
+      changeGlobalStatus(newStore, newTask.id, newTask.status);
+
       return newStore;
     });
   }
@@ -65,6 +76,9 @@ function App() {
   function addSubtask(id: string): void {
     //function addSubtask(subtask: TaskType): void {
     //console.log(subtask);
+
+    console.log("jhh");
+
     console.log(id);
     setStore((store) => {
       return {
@@ -103,8 +117,10 @@ function App() {
     });
   }
 
-  function changeStatus(id: string, value: Status): void {
+  function changeGlobalStatus(store: Store, id: string, value: Status): void {
     const targetTask = store.tasksList.get(id);
+    console.log(targetTask);
+    console.log(targetTask?.id);
     console.log("\n");
     console.log("child", value);
 
@@ -121,8 +137,9 @@ function App() {
         status: value
       };
       newTasksList.set(id, newTask);
-
+      console.log(targetTask);
       if (targetTask && targetTask.parentTaskId) {
+        console.log("yyyy");
         const parentTask = newTasksList.get(targetTask.parentTaskId);
 
         if (parentTask) {
@@ -154,6 +171,7 @@ function App() {
               ? "completed"
               : "inProgress";
 
+          console.log("parent current", parentTask.status);
           newTasksList.set(parentTask.id, parentTask);
         }
       }
@@ -169,8 +187,79 @@ function App() {
     });
 
     console.log("upload");
+  }
 
-    
+
+  function changeStatus(id: string, value: Status): void {
+    const targetTask = store.tasksList.get(id);
+    console.log(targetTask);
+    console.log(targetTask?.id);
+    console.log("\n");
+    console.log("child", value);
+
+    // add recursion
+
+    setStore((store) => {
+      const newTasksList = new Map(store.tasksList);
+      const editedTask = newTasksList.get(id);
+      if (!editedTask) {
+        throw new Error("There is no task with such id");
+      }
+      const newTask = {
+        ...editedTask,
+        status: value
+      };
+      newTasksList.set(id, newTask);
+      console.log(targetTask);
+      if (targetTask && targetTask.parentTaskId) {
+        console.log("yyyy");
+        const parentTask = newTasksList.get(targetTask.parentTaskId);
+
+        if (parentTask) {
+          let allWaitng = true;
+          let allCompleted = true;
+
+          for (const id of parentTask.subTaskIds) {
+            const subtask = newTasksList.get(id);
+            console.log(subtask);
+            if (
+              subtask?.status === "awaiting" ||
+              subtask?.status === "inProgress"
+            ) {
+              console.log("NOT C");
+              allCompleted = false;
+            }
+            if (
+              subtask?.status === "completed" ||
+              subtask?.status === "inProgress"
+            ) {
+              console.log("NOT W");
+              allWaitng = false;
+            }
+          }
+
+          parentTask.status = allWaitng
+            ? "awaiting"
+            : allCompleted
+              ? "completed"
+              : "inProgress";
+
+          console.log("parent current", parentTask.status);
+          newTasksList.set(parentTask.id, parentTask);
+        }
+      }
+
+      const newStore = {
+        ...store,
+        tasksList: newTasksList
+      };
+
+      void loadData(newStore);
+
+      return newStore;
+    });
+
+    console.log("upload");
   }
 
   function changeMode(mode: Store["appStatus"]): void {
@@ -196,45 +285,45 @@ function App() {
 
   return (
     <AppContext.Provider value={store}>
-    <div className={Styles.app}>
-      {store.appStatus === "idle" && (
-        <TasksList
-          tasks={Array.from(store.tasksList.values())}
-          deleteFunc={deleteTask}
-          changeFunc={changeStatus}
-          editFunc={editTask}
-          addSubtaskFunc={addSubtask}
-        />
-      )}
-      {(store.appStatus === "adding" ||
-        store.appStatus === "editing" ||
-        store.appStatus === "addingSubtask") && (
-        <AddingForm
-          submitFunc={addTask}
-          editableTask={store.editableTask}
-          appMode={store.appStatus}
-          parentTaskId={
-            store.appStatus === "editing" && store.editableTask
-              ? store.editableTask.parentTaskId
-              : store.appStatus === "adding"
-                ? null
-                : store.addingSubtaskId
-          }
-        />
-      )}
+      <div className={Styles.app}>
+        {store.appStatus === "idle" && (
+          <TasksList
+            tasks={Array.from(store.tasksList.values())}
+            deleteFunc={deleteTask}
+            changeFunc={changeStatus}
+            editFunc={editTask}
+            addSubtaskFunc={addSubtask}
+          />
+        )}
+        {(store.appStatus === "adding" ||
+          store.appStatus === "editing" ||
+          store.appStatus === "addingSubtask") && (
+          <AddingForm
+            submitFunc={addTask}
+            editableTask={store.editableTask}
+            appMode={store.appStatus}
+            parentTaskId={
+              store.appStatus === "editing" && store.editableTask
+                ? store.editableTask.parentTaskId
+                : store.appStatus === "adding"
+                  ? null
+                  : store.addingSubtaskId
+            }
+          />
+        )}
 
-      {store.appStatus === "idle" && (
-        <Button
-          btnText="Add"
-          handleClick={() => {
-            changeMode("adding");
-          }}
-        />
-      )}
-      <p>m: {store.appStatus}</p>
-      <p>e: {store.editableTask?.id || "null"}</p>
-      <p>a: {store.addingSubtaskId}</p>
-    </div>
+        {store.appStatus === "idle" && (
+          <Button
+            btnText="Add"
+            handleClick={() => {
+              changeMode("adding");
+            }}
+          />
+        )}
+        <p>m: {store.appStatus}</p>
+        <p>e: {store.editableTask?.id || "null"}</p>
+        <p>a: {store.addingSubtaskId}</p>
+      </div>
     </AppContext.Provider>
   );
 }
