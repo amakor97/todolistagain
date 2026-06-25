7238baab725ebd335e4bbe36749de73e5b908714
amakor97 <amakor.andrei@gmail.com>
2026-06-25 18:04:34 +0300

    [STYLE]: add some styles to task component

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -27,75 +27,78 @@ function Task({
   editFunc: editTask;
   addSubtaskFunc: addSubtask;
 }) {
-  
   const store: Store = useContext(AppContext);
 
   return (
-    <article className={`${Styles.task} ${task.status === "awaiting" ? Styles.awaiting : task.status === "inProgress" ? Styles.inProgress : Styles.completed}`}>
+    <article
+      className={`${Styles.task} ${task.status === "awaiting" ? Styles.awaiting : task.status === "inProgress" ? Styles.inProgress : Styles.completed}`}
+    >
       <h2 className={Styles.title}>title: {task.name}</h2>
       <p className={Styles.desc}>desc: {task.description}</p>
       <p className={Styles.status}>status: {task.status}</p>
       <p className={Styles.status}>parent id: {task.parentTaskId}</p>
       <p className={Styles.status}>id: {task.id}</p>
-      <p className={Styles.status}>sub task id's: {task.subTaskIds.join(", ")}</p>
-      <Button
-        btnText="Delete"
-        handleClick={() => {
-          deleteFunc(task.id);
-        }}
-      />
-      {(task.subTaskIds.length === 0) && <><Button
-        btnText="Awaiting"
-        handleClick={() => {
-          changeFunc(task.id, "awaiting");
-        }}
-      />
-      <Button
-        btnText="In progress"
-        handleClick={() => {
-          changeFunc(task.id, "inProgress");
-        }}
-      />
-      <Button
-        btnText="Completed"
-        handleClick={() => {
-          changeFunc(task.id, "completed");
-        }}
-      /></>}
-      
-      <Button
-        btnText="Edit"
-        handleClick={() => {
-          editFunc(task.id);
-        }}
-      />
-      <Button
-        btnText="Add subtask"
-        handleClick={() => {
-          console.log(task.id)
-          addSubtaskFunc(task.id);
-        }}
-      />
+      <p className={Styles.status}>
+        sub task id's: {task.subTaskIds.join(", ")}
+      </p>
+      <div className={Styles.control}>
+        <Button
+          btnText="Delete"
+          handleClick={() => {
+            deleteFunc(task.id);
+          }}
+        />
+        {task.subTaskIds.length === 0 && (
+          <>
+            <Button
+              btnText="Awaiting"
+              handleClick={() => {
+                changeFunc(task.id, "awaiting");
+              }}
+            />
+            <Button
+              btnText="In progress"
+              handleClick={() => {
+                changeFunc(task.id, "inProgress");
+              }}
+            />
+            <Button
+              btnText="Completed"
+              handleClick={() => {
+                changeFunc(task.id, "completed");
+              }}
+            />
+          </>
+        )}
 
-      {
-        task.subTaskIds.map((id) => {
+        <Button
+          btnText="Edit"
+          handleClick={() => {
+            editFunc(task.id);
+          }}
+        />
+        <Button
+          btnText="Add subtask"
+          handleClick={() => {
+            addSubtaskFunc(task.id);
+          }}
+        />
+      </div>
 
-          const subtask = store.tasksList.get(id)
+      {task.subTaskIds.map((id) => {
+        const subtask = store.tasksList.get(id);
 
-          return subtask ? (
-            <Task
-              task={subtask}
-              deleteFunc={deleteFunc}
-              changeFunc={changeFunc}
-              editFunc={editFunc}
-              addSubtaskFunc={addSubtaskFunc}
-              key={subtask.id}
-            />)
-          : null;
-})
-        }
-      
-
+        return subtask ? (
+          <Task
+            task={subtask}
+            deleteFunc={deleteFunc}
+            changeFunc={changeFunc}
+            editFunc={editFunc}
+            addSubtaskFunc={addSubtaskFunc}
+            key={subtask.id}
+          />
+        ) : null;
+      })}
     </article>
   );
 }
